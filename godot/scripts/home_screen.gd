f5dc81f8d91c0df3d64fcf90f4cd402b16966620
ItsTheMcCoy <macweible@gmail.com>
2026-06-03 02:57:07 -0500

    Bump portrait home UI scale to 1.45

--- godot/scripts/home_screen.gd
+++ godot/scripts/home_screen.gd
@@ -83,7 +83,7 @@ func _apply_responsive_layout(viewport_size: Vector2) -> void:
 	var mobile_portrait := is_mobile and not is_landscape
 	var ui_scale := 1.0
 	if mobile_portrait:
-		ui_scale = 1.35
+		ui_scale = 1.45
 
 	var home_width := 360.0
 	if mobile_landscape:
